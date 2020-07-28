3fb2b3ca92818deae669e882064ae1e7498ce02c
saragustafson <saramgustafson@gmail.com>
2020-07-28 11:51:09 -0700

    Updates

---
+++ precache-manifest.d2c44a73f0b27f08df0e205c5a76cda4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bbc2a72350a2517dc4b89b1ae5dcc74",
+    "url": "/classportfolio/index.html"
+  },
+  {
+    "revision": "c03e6b2b488e544923b4",
+    "url": "/classportfolio/static/css/main.ff162cd3.chunk.css"
+  },
+  {
+    "revision": "7ece87233ab186c330b4",
+    "url": "/classportfolio/static/js/2.7c2483fe.chunk.js"
+  },
+  {
+    "revision": "f86a4e17d3cb58f717d9f416937db9aa",
+    "url": "/classportfolio/static/js/2.7c2483fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c03e6b2b488e544923b4",
+    "url": "/classportfolio/static/js/main.15edc2bd.chunk.js"
+  },
+  {
+    "revision": "a7b75156ac751487ff1e",
+    "url": "/classportfolio/static/js/runtime-main.060667dd.js"
+  },
+  {
+    "revision": "4317be933b8b615a4b7847d40093ae3f",
+    "url": "/classportfolio/static/media/header.4317be93.jpg"
+  }
+]);
